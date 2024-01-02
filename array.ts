1953bb3ada202bb4ad5d49ff3d5c9ac72c8568b6
shahbaz4783 <shahbaz4783@gmail.com>
2024-01-03 01:59:11 +0530

    arrays

---
+++ array.ts
@@ -0,0 +1,15 @@
+// To initilize empty array, give annotation
+const cars: string[] = [];
+
+// To initalise 2D array
+const mobiles: string[][] = [];
+
+// Type inference
+const pets = ['dog', 'cat', 'goat'];
+
+const dates = [new Date(), new Date(), new Date()];
+
+// Uninon in Arrays
+const impDates: (Date | string)[] = [];
+
+const numStr: (number | string)[] = [];
